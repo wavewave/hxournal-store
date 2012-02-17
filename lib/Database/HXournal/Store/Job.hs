0d8bb9afd69dc5539aaa0966aa423ff44420a0bd
ianwookim <ianwookim@gmail.com>
2012-02-17 07:32:14 +0000

    code clean up

--- lib/Database/HXournal/Store/Job.hs
+++ lib/Database/HXournal/Store/Job.hs
@@ -1,3 +1,15 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Database.HXournal.Store.Job  
+-- Copyright   : (c) 2012 Ian-Woo Kim
+--
+-- License     : BSD3
+-- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
+-- Stability   : experimental
+-- Portability : GHC
+--
+-----------------------------------------------------------------------------
+
 module Database.HXournal.Store.Job where
 
 import Control.Monad
@@ -5,26 +17,28 @@ import Data.UUID
 import Database.HXournal.Store.Config 
 import Data.Maybe
 import Text.Xournal.Parse
-
 import Application.XournalConvert.Convert.MakeSVG
-
 import System.Directory 
 import System.Exit 
 import System.FilePath
 import System.Process
 import System.Posix.Files 
 
+-- | 
 
 flipMaybe :: Maybe a -> b -> ( a-> b) -> b
 flipMaybe m f s = maybe f s m  
 
+-- | 
+
 startAdd :: String -> FilePath -> IO Int
 startAdd uuidstr file = do 
-    putStrLn "job started"
+    -- putStrLn "job started"
     mhxojstoreconf <- loadConfigFile >>= getHXournalStoreConfiguration
-
     flipMaybe mhxojstoreconf (error "cannot parse config file") 
       $ \hc -> checkUUIDNUpdate hc uuidstr 0 file generateAction
+
+-- | 
 
 startNewVersion :: String -> Int -> FilePath -> IO Int
 startNewVersion uuidstr nver file = do 
@@ -37,6 +51,7 @@ startNewVersion uuidstr nver file = do
     flipMaybe mhxojstoreconf (error "cannot parse config file") 
       $ \hc -> checkUUIDNUpdate hc uuidstr nver file generateAction
 
+-- | 
                 
 checkUUIDNUpdate :: HXournalStoreConfiguration 
                  -> String  -- ^ UUID string
@@ -61,8 +76,7 @@ checkUUIDNUpdate hc uuidstr ver file action = do
             createDirectory uuiddir
             action uuiddir ver file
                
-
-
+-- | 
 
 generateAction :: FilePath -> Int -> FilePath -> IO Int
 generateAction uuiddir ver xojfile = do 
@@ -89,57 +103,23 @@ generateAction uuiddir ver xojfile = do
       forM_ numpages $ \(num,name) -> do
         putStrLn $ "ln -s " ++ "../data" </> name ++ " " ++ show num ++ ".svg"
         system $ "ln -s " ++ "../data" </> name ++ " " ++ show num ++ ".svg"
-
+      -- 
       setCurrentDirectory uuiddir 
       removelatestlink 
-
+      -- 
       system $ "ln -s " ++ verstr ++ " latest"
       system $ "ln -s " ++ (verstr  </> "data" </> filename_wo_dir) ++ " latest.xoj"
       return (length pages)
     else error $ "directory " ++ verstr ++ " exist"
       
+-- | 
+
 removelatestlink :: IO () 
 removelatestlink = do 
   b <- doesDirectoryExist "latest"
   when b $ removeLink "latest"
   b2 <- doesFileExist "latest.xoj"
   when b2 $ removeLink "latest.xoj"
-
-
-{-       
-generateAction :: FilePath -> FilePath -> IO Int
-generateAction uuiddir xojfile = do 
-  createDirectory uuiddir 
-  setCurrentDirectory uuiddir 
-  createDirectory "v0"
-  let newvdir = uuiddir </> "v0"
-      newvdatadir = newvdir </> "data"
-  setCurrentDirectory newvdir
-  createDirectory "data"
-  setCurrentDirectory newvdatadir
-  let filename_wo_dir = takeFileName xojfile 
-  copyFile xojfile (newvdatadir </> filename_wo_dir)
-  xojcontent <- checkIfBinary xojfile >>= \b -> 
-                  ifThenElse b (read_xojgz xojfile) (read_xournal xojfile)
-
-  pages <- makeSVGFromXournal xojcontent xojfile newvdatadir
-  let numpages = zip [1..] pages :: [(Int,String)] 
-  createDirectory (newvdir </> "page")
-  setCurrentDirectory (newvdir </> "page") 
-  forM_ numpages $ \(num,name) -> 
-    system $ "ln -s " ++ newvdatadir </> name ++ " " ++ show num ++ ".svg"
-
-  setCurrentDirectory uuiddir 
-  system $ "ln -s v0 latest"
-  system $ "ln -s " ++ ("v0" </> "data" </> filename_wo_dir) ++ " latest.xoj"
-  return (length pages)
--}
-
-
-
-
-
-
 
 
 
